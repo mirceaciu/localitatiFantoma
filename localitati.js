749b21bcfc4f37afef67e5edb496d18012b52325
Stanciu Bogdan Mircea <mirceaciu@gmail.com>
2015-06-25 16:34:16 +0300

    proper geojson
    fisierul geojson cu cap de tabel necesar afisarii. am adaugat si un
    fisier geojson creat din qgis pentru comparatie

--- localitati.js
+++ localitati.js
@@ -3,22 +3,49 @@ var map = L.map('map').setView([45.986, 24.785], 7);
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> &amp; <a href="http://geo-spatial.org">Geo-spatial.org</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>, Map by Mirceaciu',
     zoomControl: false,
-    maxZoom: 7,
-    minZoom: 6,
+    maxZoom: 18,
+    //minZoom: 6,
     id: 'mirceaciu.6caff4a4',
     accessToken: 'pk.eyJ1IjoibWlyY2VhY2l1IiwiYSI6IjNkNGFiMTU5NjRlNGNkZTA1ZGExMDVkNjUxYzZmZDlhIn0.5E0fCQOJlyAJFjkEYX1NGg'
 }).addTo(map);
 
-    map.dragging.disable();
-    map.touchZoom.disable();
-    map.doubleClickZoom.disable();
-    map.scrollWheelZoom.disable();
-if (map.tap) map.tap.disable();
+    //map.dragging.disable();
+    //map.touchZoom.disable();
+    //map.doubleClickZoom.disable();
+    //map.scrollWheelZoom.disable();
+//if (map.tap) map.tap.disable();
+
+var iconita = new L.icon({
+    iconUrl: 'C:/Users/mircea.stanciu/Documents/GitHub/localitatiFantoma/images/fantoma.png',
+    //iconRetinaUrl: 'my-icon@2x.png',
+    //iconSize: [16, 16],
+    //iconAnchor: [22, 94],
+    //popupAnchor: [-3, -76],
+    //shadowUrl: 'my-icon-shadow.png',
+    //shadowRetinaUrl: 'my-icon-shadow@2x.png',
+    //shadowSize: [68, 95],
+    //shadowAnchor: [22, 94]
+});
 
 function puncteLocalitati(feature, layer){
-	layer.bindPopup("Buuuuuu!")
+	layer.bindPopup("Buuuuuu!");
+	layer.setIcon(iconita);
 };
 
 L.geoJson(localitati, {
 	onEachFeature: puncteLocalitati
+}).addTo(map).bringToFront();
+
+/*
+L.geoJson(exp_fantome, {
+	onEachFeature: puncteLocalitati
 }).addTo(map);
+*/
+
+
+/*
+L.geoJson(judete, {
+	onEachFeature: puncteLocalitati
+}).addTo(map);
+
+*/
