668ab72a3633da91b28c105b49cc23571b55d33e
Stanciu Bogdan Mircea <mirceaciu@gmail.com>
2015-07-04 19:13:56 +0300

    new-font
    import font, stilizat un pic casutele de informatii

--- localitati.js
+++ localitati.js
@@ -3,8 +3,8 @@ var map = L.map('map').setView([45.986, 24.785], 7);
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> &amp; <a href="http://geo-spatial.org">Geo-spatial.org</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>, Map by Mirceaciu',
     zoomControl: false,
-    maxZoom: 18,
-    //minZoom: 6,
+    maxZoom: 10,
+    minZoom: 6,
     id: 'mirceaciu.6caff4a4',
     accessToken: 'pk.eyJ1IjoibWlyY2VhY2l1IiwiYSI6IjNkNGFiMTU5NjRlNGNkZTA1ZGExMDVkNjUxYzZmZDlhIn0.5E0fCQOJlyAJFjkEYX1NGg'
 }).addTo(map);
@@ -12,7 +12,7 @@ var map = L.map('map').setView([45.986, 24.785], 7);
 
 
 
-var slideshow = '<embed type="application/x-shockwave-flash" src="https://photos.gstatic.com/media/slideshow.swf" width="100%" height="300px" flashvars="host=picasaweb.google.com&hl=en_US&feat=flashalbum&RGB=0x000000&feed=https%3A%2F%2Fpicasaweb.google.com%2Fdata%2Ffeed%2Fapi%2Fuser%2F117058608469882484167%2Falbumid%2F6150460782771452641%3Falt%3Drss%26kind%3Dphoto%26hl%3Den_US" pluginspage="http://www.macromedia.com/go/getflashplayer"></embed>'
+var slideshow = '<embed type="application/x-shockwave-flash" src="https://photos.gstatic.com/media/slideshow.swf" width="100%" height="300" flashvars="host=picasaweb.google.com&hl=en_GB&feat=flashalbum&RGB=0x000000&feed=https%3A%2F%2Fpicasaweb.google.com%2Fdata%2Ffeed%2Fapi%2Fuser%2F101308797789854061175%2Falbumid%2F6164681416049640049%3Falt%3Drss%26kind%3Dphoto%26hl%3Den_GB" pluginspage="http://www.macromedia.com/go/getflashplayer"></embed>'
     //map.dragging.disable();
     //map.touchZoom.disable();
     //map.doubleClickZoom.disable();
@@ -20,7 +20,7 @@ var slideshow = '<embed type="application/x-shockwave-flash" src="https://photos
 //if (map.tap) map.tap.disable();
 
 var iconita = new L.icon({
-    iconUrl: 'C:/Users/mircea.stanciu/Documents/GitHub/localitatiFantoma/images/fantoma.png',
+    iconUrl: 'images/gimmi.png',
     //iconRetinaUrl: 'my-icon@2x.png',
     iconSize: [16, 16],
    //iconAnchor: [0, 0],
@@ -31,18 +31,18 @@ var iconita = new L.icon({
     //shadowAnchor: [2, 94]
 });
 
-function puncteLocalitati(feature, layer){
-	layer.bindPopup("Buuuuuu!");
-	layer.setIcon(iconita);
-};
+
 
 L.geoJson(localitati, {
-	//onEachFeature: puncteLocalitati;
+	
+    
+
     onEachFeature: function (feature, layer) {
-        //layer.bindPopup(feature.properties.GPSUserName);
+        layer.bindPopup(feature.properties.Name);
+        layer.setIcon(iconita);
 
         layer.on('click', function (e) {
-            document.getElementById("info").innerHTML = "aici vom scrie informatii despre " + feature.properties.Name + ", iar mai jos imagini, daca exista" 
+            document.getElementById("info").innerHTML = "<p>aici vom scrie informatii despre " + feature.properties.Name + ", iar mai jos imagini, daca exista</p>" + "<br>"+ "<p> pentru imagini se vor da credite autorului tot aici gen:</p><p><a href=''> Autor: Andrea Dumitrache</a><p/>"  
             document.getElementById("meta").innerHTML = slideshow;
            
            // $("#feature_infos").stop();
