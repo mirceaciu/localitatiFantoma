532bcd327d835677051bf024060c38d0e47a2183
Stanciu Bogdan Mircea <mirceaciu@gmail.com>
2015-07-09 01:02:01 +0300

    slider Nav
    am adaugat si stizilat navigarea in imaginile din slider

--- localitati.js
+++ localitati.js
@@ -47,19 +47,19 @@ L.geoJson(localitati, {
             document.getElementById("info").innerHTML = "<p>aici vom scrie informatii despre " + feature.properties.name + ", iar mai jos imagini, daca exista</p>" + "<br>"+ "<p> pentru imagini se vor da credite autorului tot aici gen:</p><p><a href=''> Autor: Andrea Dumitrache</a><p/>"  
             document.getElementById("meta").innerHTML = slideshow;
             $(".rslides").responsiveSlides({
-              auto: true,             // Boolean: Animate automatically, true or false
+              auto: false,             // Boolean: Animate automatically, true or false
               speed: 500,            // Integer: Speed of the transition, in milliseconds
               timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
               pager: false,           // Boolean: Show pager, true or false
               nav: true,             // Boolean: Show navigation, true or false
-              random: false,          // Boolean: Randomize the order of the slides, true or false
-              pause: false,           // Boolean: Pause on hover, true or false
-              pauseControls: true,    // Boolean: Pause when hovering controls, true or false
-              prevText: "Previous",   // String: Text for the "previous" button
-              nextText: "Next",       // String: Text for the "next" button
-              maxwidth: "",           // Integer: Max-width of the slideshow, in pixels
-              navContainer: "",       // Selector: Where controls should be appended to, default is after the 'ul'
-              manualControls: "",     // Selector: Declare custom pager navigation
+              //random: false,          // Boolean: Randomize the order of the slides, true or false
+              //pause: false,           // Boolean: Pause on hover, true or false
+              //pauseControls: true,    // Boolean: Pause when hovering controls, true or false
+              //prevText: "Previous",   // String: Text for the "previous" button
+              //nextText: "Next",       // String: Text for the "next" button
+              //maxwidth: "",           // Integer: Max-width of the slideshow, in pixels
+              //navContainer: "",       // Selector: Where controls should be appended to, default is after the 'ul'
+              //manualControls: "",     // Selector: Declare custom pager navigation
               namespace: "rslides",   // String: Change the default namespace used
               before: function(){},   // Function: Before callback
               after: function(){}     // Function: After callback
