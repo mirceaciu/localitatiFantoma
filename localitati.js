1460b577a15fd266c000e71d9f43e5d9c17e7039
Stanciu Bogdan Mircea <Mircea.Stanciu@SORINRU-DSK1.intranet.teamnet.ro>
2015-07-23 10:55:22 +0300

    minor
    basemap cu denumiri reprezentative

--- localitati.js
+++ localitati.js
@@ -36,7 +36,7 @@ var  cuBaseMap  = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.
 
  transparent =   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> &amp; <a href="http://geo-spatial.org">Geo-spatial.org</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>, Map by Mirceaciu',
-    maxZoom: 10,
+    maxZoom: 17,
     minZoom: 6,
     id: 'mirceaciu.ml2704el', // transparent
     accessToken: 'pk.eyJ1IjoibWlyY2VhY2l1IiwiYSI6IjNkNGFiMTU5NjRlNGNkZTA1ZGExMDVkNjUxYzZmZDlhIn0.5E0fCQOJlyAJFjkEYX1NGg'
@@ -205,8 +205,8 @@ var JudetePoly = L.geoJson(judete, {
 
 
 var baseMaps = {
-    "FrumoasaRom": transparent,
-    "BasemapMapBox": cuBaseMap
+    "Base-map": transparent,
+    "Satellite": cuBaseMap
     
 };
 
